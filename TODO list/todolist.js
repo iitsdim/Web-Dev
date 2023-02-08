2423acfdd778fff2860583ff586175fb484e2a41
iitsdim <dimash241@gmail.com>
2023-02-08 11:52:56 +0600

    Added done list

--- TODO list/todolist.js
+++ TODO list/todolist.js
@@ -1,13 +1,3 @@
-// Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName("LI");
-for (let i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
 // Click on a close button to hide the current list item
 let close = document.getElementsByClassName("close");
 for (let i = 0; i < close.length; i++) {
@@ -18,17 +8,17 @@ for (let i = 0; i < close.length; i++) {
 }
 
 // Create a new list item when clicking on the "Add" button
-function newElement() {
+const newElement = function (listName) {
     let li = document.createElement("LI");
-    let inputValue = document.getElementById("myInput").value;
+    let inputValue = document.getElementById("titleToDo").value;
     let t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
         alert("You must write something!");
     } else {
-        document.getElementById("toDoListDisplay").appendChild(li);
+        document.getElementById(listName.toString()).appendChild(li);
     }
-    document.getElementById("myInput").value = "";
+    document.getElementById("titleToDo").value = "";
 
     let span = document.createElement("SPAN");
     let txt = document.createTextNode("\u00D7");
@@ -43,3 +33,14 @@ function newElement() {
         }
     }
 }
+
+document.getElementById('toDoListDisplay').addEventListener('click', function (ev) {
+    if (ev.target.tagName == 'LI') {
+        document.getElementById('toDoListDone').appendChild(ev.target);
+    }
+}, false);
+document.getElementById('toDoListDone').addEventListener('click', function (ev) {
+    if (ev.target.tagName == 'LI') {
+        document.getElementById('toDoListDisplay').appendChild(ev.target);
+    }
+}, false);
