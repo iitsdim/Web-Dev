e0e5cd82238adc5d6474f916c5860ab742bafc1b
iitsdim <dimash241@gmail.com>
2023-02-16 11:51:22 +0600

    added Product alerts/product lists

---
+++ demo/src/app/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {Product} from "../product.model";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      {'name': 'BigMac', 'description': 'BIG and TASTY', 'rating': 5}
+    ];
+  }
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
