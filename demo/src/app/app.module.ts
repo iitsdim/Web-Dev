c934f2a47cab155b6436e6f487056e8d3e7cf572
iitsdim <dimash241@gmail.com>
2023-02-20 16:06:19 +0600

    Adding form/Deploying

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import {HttpClientModule} from "@angular/common/http";
 import { ShippingComponent } from './shipping/shipping.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -27,11 +28,12 @@ import { ShippingComponent } from './shipping/shipping.component';
     HttpClientModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent},
-      { path: 'shipping', component: ShippingComponent},
-    ])
+      {path: '', component: ProductListComponent},
+      {path: 'products/:productId', component: ProductDetailsComponent},
+      {path: 'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent},
+    ]),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
