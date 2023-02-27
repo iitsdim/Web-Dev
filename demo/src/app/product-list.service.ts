fbb1851a0940989e3e0c8bf14b31709bbe12e017
iitsdim <dimash241@gmail.com>
2023-02-27 14:27:56 +0600

    Added more data/refactor minor in styling

--- demo/src/app/product-list.service.ts
+++ demo/src/app/product-list.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {Product} from "./product.model";
-import {HttpClient} from "@angular/common/http";
 import {Category} from "./category.model";
 
 @Injectable({
@@ -108,7 +107,119 @@ export class ProductListService {
       "name": "Laptop Asus TUF",
       "rating": 4.5,
     },
+    {
+      "category": 1,
+      "name": "Qwatt Q43YK-MB ",
+      "description": "Телевизор Qwatt Q43YK-MB 109 см черный",
+      "rating": 5,
+      "id": 11,
+      "kaspiLink": "https://kaspi.kz/shop/p/qwatt-q43yk-mb-109-sm-chernyi-103318385/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hf2/h9a/51443504971806/qwatt-q43yk-mb-chernyi-103318385-1.jpg",
+      "likes": 33,
+    },
+    {
+      "category": 3,
+      "name": "Apple Airpods",
+      "description": "Наушники Apple AirPods with Charging Case белый",
+      "rating": 5,
+      "id": 12,
+      "kaspiLink": "https://kaspi.kz/shop/p/apple-airpods-with-charging-case-belyi-4804056/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb6/h3d/46637140508702/apple-airpods-2-with-charging-case-belyj-4804056-2-Container.jpg",
+      "likes": 5922,
+    },
+    {
+      "category": 3,
+      "name": "Apple AirPods Pro 2nd",
+      "description": "Наушники Apple AirPods Pro 2nd generation белый",
+      "rating": 5,
+      "id": 13,
+      "kaspiLink": "https://kaspi.kz/shop/p/apple-airpods-pro-2nd-generation-belyi-106362968/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hba/hf8/62281477259294/apple-airpods-pro-2nd-generation-belyj-106362968-1.jpg",
+      "likes": 19,
+    },
+    {
+      "category": 3,
+      "name": "Apple Airpods 3",
+      "description": "Наушники Apple AirPods 3 белый\n",
+      "rating": 5,
+      "id": 14,
+      "kaspiLink": "https://kaspi.kz/shop/p/apple-airpods-3-belyi-102667744/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h12/h12/46719106023454/apple-airpods-3-belyj-102667744-1.jpg",
+      "likes": 1179,
+    },
+    {
+      "category": 4,
+      "name": "Био-C",
+      "description": "Йогурт Foodmaster Био-С ваниль 3.2% 1000 г\n",
+      "rating": 5,
+      "id": 15,
+      "kaspiLink": "https://kaspi.kz/shop/p/iogurt-foodmaster-bio-s-vanil-3-2-1000-g-100231108/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h5b/h36/46693307744286/foodmaster-bio-s-kefir-3-2-1000-ml-100231108-1.jpg",
+      "likes": 48,
+    },
+    {
+      "category": 4,
+      "name": "Danone yogurt",
+      "description": "https://kaspi.kz/shop/p/iogurt-danone-klubnika-2-5-4-sht-po-120-g-101148576/?c=750000000#!/item",
+      "rating": 5,
+      "id": 16,
+      "kaspiLink": "https://kaspi.kz/shop/p/iogurt-danone-klubnika-2-5-4-sht-po-120-g-101148576/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd8/h08/62391331455006/iogurt-danone-klubnika-2-5-4-sht-120-g-101148576-1.jpg",
+      "likes": 6,
+    },
+    {
+      "category": 4,
+      "name": "Foodmaster",
+      "description": "Йогурт Foodmaster лесные ягоды 2% 900 г",
+      "rating": 5,
+      "id": 17,
+      "kaspiLink": "https://kaspi.kz/shop/p/iogurt-foodmaster-lesnye-jagody-2-900-g-102038251/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h10/h2f/46920423735326/iogurt-foodmaster-lesnye-yagody-2-900-g-102038251-1.jpg",
+      "likes": 11,
+    },
+    {
+      "category": 4,
+      "name": "Actimel",
+      "description": "Йогурт Actimel банан, клубника 30% 95 г",
+      "rating": 5,
+      "id": 18,
+      "kaspiLink": "https://kaspi.kz/shop/p/iogurt-actimel-banan-klubnika-30-95-g-105701334/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7f/h4e/52274314248222/iogurt-danone-actimel-banan-klubnika-30-95-g-105701334-1.jpg",
+      "likes": 1,
+    },
+    {
+      "category": 4,
+      "name": "Fruttis",
+      "description": "Йогурт Fruttis клубника 5% 290 г",
+      "rating": 5,
+      "id": 19,
+      "kaspiLink": "https://kaspi.kz/shop/p/iogurt-fruttis-klubnika-5-290-g-100233142/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h72/h8a/49425354391582/iogurt-campina-fruttis-klubnika-5-290-g-100233142-1.jpg",
+      "likes": 1,
+    },
+    {
+      "category": 3,
+      "name": "Celebrat A24",
+      "description": "Наушники Celebrat A24 черный",
+      "rating": 5,
+      "id": 20,
+      "kaspiLink": "https://kaspi.kz/shop/p/celebrat-a24-chernyi-101610597/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd1/h5c/34161147215902/celebrat-a24-cernyj-101610597-1-Container.jpg",
+      "likes": 45,
+    },
+    {
+      "category": 3,
+      "name": "JBL Tune 510BT",
+      "description": "Наушники JBL Tune 510BT черный\n",
+      "rating": 5,
+      "id": 21,
+      "kaspiLink": "https://kaspi.kz/shop/p/jbl-tune-510bt-chernyi-101420081/?c=750000000#!/item",
+      "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3d/h1d/33957524537374/jbl-tune-510bt-cernyj-101420081-1-Container.jpg",
+      "likes": 367,
+    },
+
     // {
+    //   "category": 4,
     //   "name": "",
     //   "description": "",
     //   "rating": 5,
@@ -127,12 +238,18 @@ export class ProductListService {
     {
       "title": "Laptop",
       "id": 2
+    },
+    {
+      "title": "Headphones",
+      "id": 3
+    },
+    {
+      "title": "Food",
+      "id": 4
     }
   ];
 
-  constructor(
-    private http: HttpClient
-  ) {
+  constructor() {
   }
 
   addToCart(product: Product) {
@@ -141,7 +258,7 @@ export class ProductListService {
 
   deleteProduct(productId: number) {
     const index = this.products.findIndex((product: Product) => product.id == productId, 0);
-    if(index > -1) {
+    if (index > -1) {
       this.products.splice(index, 1);
     }
   }
@@ -155,7 +272,7 @@ export class ProductListService {
   }
 
   getProductsByCategory(categoryId: number | undefined) {
-    if(!categoryId) {
+    if (!categoryId) {
       return this.products;
     }
     return this.products.filter((product: Product) => product.category === categoryId)
