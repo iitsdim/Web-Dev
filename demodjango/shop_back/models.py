7152d6e09c0c110e2cb61636c3708e2b61059606
iitsdim <dimash241@gmail.com>
2023-03-29 07:37:43 +0600

    Added models

--- demodjango/shop_back/models.py
+++ demodjango/shop_back/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    categories = models.ManyToManyField(Category)
